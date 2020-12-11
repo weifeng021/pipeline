5fbc646b696380bc148489db42a73ec4739c9434
weifeng021 <70249850+weifeng021@users.noreply.github.com>
2020-12-11 09:30:06 +0000

    code react

--- server.js
+++ server.js
@@ -6,7 +6,6 @@ http.createServer(function (request, response) {
     // HTTP 状态值: 200 : OK
     // 内容类型: text/plain
     response.writeHead(200, {'Content-Type': 'text/plain'});
-
     // 发送响应数据 "Hello World"
     response.end('Hello World\n');
 }).listen(8888);
